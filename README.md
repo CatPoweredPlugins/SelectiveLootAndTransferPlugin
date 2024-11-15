3d4c2c0b45ed96971954278ae680d5da0f3c4201
Rudokhvist <kotlyar.andrey@gmail.com>
2024-11-15 12:29:15 +0200

    add commands to send items from specified appids only

--- README.md
+++ README.md
@@ -7,32 +7,36 @@ This plugin allows to send community inventory items of given types from bot to 
 - this plugin is only *guaranteed* to work with ASF-generic. If you use any other variant and encounter any issues - first you are encouraged to switch to ASF-generic, and only if it does not help - report an issue.
 - download `SelectiveLootAndTransferPlugin.zip` file from [latest release](https://github.com/CatPoweredPlugins/SelectiveLootAndTransferPlugin/releases/latest) page.
 - unpack downloaded .zip file to `plugins` folder inside your ASF folder, together with included folder.
-- (re)start ASF, you should get a message indicating that plugin loaded successfully. 
+- (re)start ASF, you should get a message indicating that plugin loaded successfully.
 
 ## Usage
 Plugin implements following commands:
 
-Command | Access | Description
---- | --- | ---
-`transfer# <Bots> <Modes> <Bot>` | `Master` | Sends from given bot instances to given `Bot` instance, all inventory items that are matching given `modes`, explained **[below](#modes-parameter)**.
-`loot# <Bots> <Modes>` | `Master` | Sends all inventory items that are matching given `modes`, explained **[below](#modes-parameter)**, from given bot instances to `Master` user defined in their `SteamUserPermissions` (with lowest steamID if more than one).
-`transferm <Bots> <Modes> <Bot>` | `Master` | Same as `transfer#`, but only **Marketable** items are sent.
-`lootm <Bots> <Modes>` | `Master` | Same as `loot#`, but only **Marketable** items are sent.
+| Command                                    | Access   | Description                                                                                                                                                                                                                                       |
+|--------------------------------------------|----------|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `transfer# <Bots> <Modes> <Bot>`           | `Master` | Sends from given bot instances to given `Bot` instance, all inventory items that are matching given `modes`, explained **[below](#modes-parameter)**.                                                                                             |
+| `loot# <Bots> <Modes>`                     | `Master` | Sends all inventory items that are matching given `modes`, explained **[below](#modes-parameter)**, from given bot instances to `Master` user defined in their `SteamUserPermissions` (with lowest steamID if more than one).                     |
+| `transfer+ <Bots> <Modes> <Appids> <Bot>`  | `Master` | Sends from given bot instances to given `Bot` instance, all inventory items that are matching given `modes`, explained **[below](#modes-parameter)**, from apps `Appids`.                                                                         |
+| `loot+ <Bots> <Modes> <Appids>`            | `Master` | Sends all inventory items that are matching given `modes`, explained **[below](#modes-parameter)**, from apps `Appids`, from given bot instances to `Master` user defined in their `SteamUserPermissions` (with lowest steamID if more than one). |
+| `transferm <Bots> <Modes> <Bot>`           | `Master` | Same as `transfer#`, but only **Marketable** items are sent.                                                                                                                                                                                      |
+| `lootm <Bots> <Modes>`                     | `Master` | Same as `loot#`, but only **Marketable** items are sent.                                                                                                                                                                                          |
+| `transferm+ <Bots> <Modes> <Appids> <Bot>` | `Master` | Same as `transfer+`, but only **Marketable** items are sent.                                                                                                                                                                                      |
+| `lootm+ <Bots> <Modes> <Appids>`           | `Master` | Same as `loot+`, but only **Marketable** items are sent.                                                                                                                                                                                          |
 
 ## `Modes` parameter
 
 `<Modes>` argument accepts multiple mode values, separated as usual by a comma. Available mode values are specified below:
 
-Value | Alias | Description
---- | --- | ---
-All | A | Same as enabling all item types below
-Background | BG | Profile background to use on your Steam profile
-Booster | BO | Booster pack
-Card | C | Steam trading card, being used for crafting badges (non-foil)
-Emoticon | E | Emoticon to use in Steam Chat
-Foil | F | Foil variant of `Card`
-Gems | G | Steam gems being used for crafting boosters, sacks included
-Unknown | U | Every type that doesn't fit in any of the above
+| Value      | Alias | Description                                                   |
+|------------|-------|---------------------------------------------------------------|
+| All        | A     | Same as enabling all item types below                         |
+| Background | BG    | Profile background to use on your Steam profile               |
+| Booster    | BO    | Booster pack                                                  |
+| Card       | C     | Steam trading card, being used for crafting badges (non-foil) |
+| Emoticon   | E     | Emoticon to use in Steam Chat                                 |
+| Foil       | F     | Foil variant of `Card`                                        |
+| Gems       | G     | Steam gems being used for crafting boosters, sacks included   |
+| Unknown    | U     | Every type that doesn't fit in any of the above               |
 
 For example, in order to send trading cards and foils from `MyBot` to `MyMain`, you'd execute:
 
--- SelectiveLootAndTransferPlugin/SelectiveLootAndTransferPlugin.cs
+++ SelectiveLootAndTransferPlugin/SelectiveLootAndTransferPlugin.cs
@@ -14,7 +14,6 @@ using System.Globalization;
 using JetBrains.Annotations;
 using ArchiSteamFarm.Web.GitHub;
 
-
 namespace SelectiveLootAndTransferPlugin {
 #pragma warning disable CA1863 // VS, please, fuck off with this 'CompositeFormat' bullshit;
 #pragma warning disable CA1812 // ASF uses this class during runtime
@@ -44,13 +43,13 @@ namespace SelectiveLootAndTransferPlugin {
 
 			Version newVersion = new(releaseResponse.Tag);
 
-			if (!(Version.Major == newVersion.Major && Version.Minor == newVersion.Minor && Version.Build == newVersion.Build) && !(asfUpdate || forced)) {
+			if (!((Version.Major == newVersion.Major) && (Version.Minor == newVersion.Minor) && (Version.Build == newVersion.Build)) && !(asfUpdate || forced)) {
 				ASF.ArchiLogger.LogGenericInfo(string.Format(CultureInfo.CurrentCulture, "New {0} plugin version {1} is only compatible with latest ASF version", Name, newVersion));
+
 				return null;
 			}
 
-
-			if (Version >= newVersion & !forced) {
+			if ((Version >= newVersion) & !forced) {
 				ASF.ArchiLogger.LogGenericInfo(string.Format(CultureInfo.CurrentCulture, Strings.PluginUpdateNotFound, Name, Version, newVersion));
 
 				return null;
@@ -85,22 +84,32 @@ namespace SelectiveLootAndTransferPlugin {
 				"TRANSFER#" when args.Length > 2 => await ResponseTransfer(bot, access, args[1], args[2]).ConfigureAwait(false),
 				"LOOT#" when args.Length > 2 => await ResponseLoot(access, steamID, args[1], Utilities.GetArgsAsText(args, 2, ",")).ConfigureAwait(false),
 				"LOOT#" => await ResponseLoot(bot, access, args[1]).ConfigureAwait(false),
+				"TRANSFER+" when args.Length > 4 => await ResponseTransferApp(access, steamID, args[1], args[2], args[3], Utilities.GetArgsAsText(args, 3, ",")).ConfigureAwait(false),
+				"TRANSFER+" when args.Length > 3 => await ResponseTransferApp(bot, access, args[1], args[2], args[3]).ConfigureAwait(false),
+				"LOOT+" when args.Length > 3 => await ResponseLootApp(access, steamID, args[1], args[2], Utilities.GetArgsAsText(args, 2, ",")).ConfigureAwait(false),
+				"LOOT+" when args.Length > 2 => await ResponseLootApp(bot, access, args[1], args[2]).ConfigureAwait(false),
 				"TRANSFERM" when args.Length > 3 => await ResponseTransfer(access, steamID, args[1], args[2], Utilities.GetArgsAsText(args, 3, ","), false).ConfigureAwait(false),
 				"TRANSFERM" when args.Length > 2 => await ResponseTransfer(bot, access, args[1], args[2], false).ConfigureAwait(false),
 				"LOOTM" when args.Length > 2 => await ResponseLoot(access, steamID, args[1], Utilities.GetArgsAsText(args, 2, ","), false).ConfigureAwait(false),
 				"LOOTM" => await ResponseLoot(bot, access, args[1], false).ConfigureAwait(false),
+				"TRANSFERM+" when args.Length > 4 => await ResponseTransferApp(access, steamID, args[1], args[2], args[3], Utilities.GetArgsAsText(args, 3, ","), false).ConfigureAwait(false),
+				"TRANSFERM+" when args.Length > 3 => await ResponseTransferApp(bot, access, args[1], args[2], args[3], false).ConfigureAwait(false),
+				"LOOTM+" when args.Length > 3 => await ResponseLootApp(access, steamID, args[1], args[2], Utilities.GetArgsAsText(args, 2, ","), false).ConfigureAwait(false),
+				"LOOTM+" when args.Length > 2 => await ResponseLootApp(bot, access, args[1], args[2], false).ConfigureAwait(false),
 				_ => null,
 			};
 		}
 
 		public Task OnLoaded() {
 			ASF.ArchiLogger.LogGenericInfo("Selective Loot and Transfer Plugin by Rudokhvist, powered by ginger cats");
+
 			return Task.CompletedTask;
 		}
 
 		private static async Task<string?> ResponseTransfer(Bot bot, EAccess access, string mode, string botNameTo, bool sendNotMarketable = true) {
 			if (string.IsNullOrEmpty(botNameTo) || string.IsNullOrEmpty(mode)) {
 				ASF.ArchiLogger.LogNullError(null, nameof(mode) + " || " + nameof(botNameTo));
+
 				return null;
 			}
 
@@ -113,6 +122,7 @@ namespace SelectiveLootAndTransferPlugin {
 			}
 
 			Bot? targetBot = Bot.GetBot(botNameTo);
+
 			if (targetBot == null) {
 				return access >= EAccess.Owner ? bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNameTo)) : null;
 			}
@@ -128,9 +138,7 @@ namespace SelectiveLootAndTransferPlugin {
 			string[] modes = mode.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
 
 			if (modes.Length == 0) {
-
 				return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsEmpty, nameof(modes)));
-
 			}
 
 			HashSet<EAssetType> transferTypes = [];
@@ -147,30 +155,37 @@ namespace SelectiveLootAndTransferPlugin {
 					case "BG":
 					case "BACKGROUND":
 						transferTypes.Add(EAssetType.ProfileBackground);
+
 						break;
 					case "BO":
 					case "BOOSTER":
 						transferTypes.Add(EAssetType.BoosterPack);
+
 						break;
 					case "C":
 					case "CARD":
 						transferTypes.Add(EAssetType.TradingCard);
+
 						break;
 					case "E":
 					case "EMOTICON":
 						transferTypes.Add(EAssetType.Emoticon);
+
 						break;
 					case "F":
 					case "FOIL":
 						transferTypes.Add(EAssetType.FoilTradingCard);
+
 						break;
 					case "G":
 					case "GEMS":
 						transferTypes.Add(EAssetType.SteamGems);
+
 						break;
 					case "U":
 					case "UNKNOWN":
 						transferTypes.Add(EAssetType.Unknown);
+
 						break;
 					default:
 						return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, mode));
@@ -185,10 +200,12 @@ namespace SelectiveLootAndTransferPlugin {
 		private static async Task<string?> ResponseTransfer(EAccess access, ulong steamID, string botNames, string mode, string botNameTo, bool sendNotMarketable = true) {
 			if (string.IsNullOrEmpty(botNames) || string.IsNullOrEmpty(mode) || string.IsNullOrEmpty(botNameTo)) {
 				ASF.ArchiLogger.LogNullError(null, nameof(botNames) + " || " + nameof(mode) + " || " + nameof(botNameTo));
+
 				return null;
 			}
 
 			HashSet<Bot>? bots = Bot.GetBots(botNames);
+
 			if ((bots == null) || (bots.Count == 0)) {
 				return access >= EAccess.Owner ? Commands.FormatStaticResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNames)) : null;
 			}
@@ -199,6 +216,7 @@ namespace SelectiveLootAndTransferPlugin {
 			switch (ASF.GlobalConfig?.OptimizationMode) {
 				case GlobalConfig.EOptimizationMode.MinMemoryUsage:
 					results = new List<string?>(bots.Count);
+
 					foreach (Task<string?> task in tasks) {
 						results.Add(await task.ConfigureAwait(false));
 					}
@@ -206,14 +224,165 @@ namespace SelectiveLootAndTransferPlugin {
 					break;
 				default:
 					results = [.. await Task.WhenAll(tasks).ConfigureAwait(false)];
+
 					break;
 			}
 
-			List<string?> responses = new(results.Where(result => !string.IsNullOrEmpty(result)));
+			List<string?> responses = [..results.Where(static result => !string.IsNullOrEmpty(result))];
+
+			return responses.Count > 0 ? string.Join("", responses) : null;
+		}
+
+		private static async Task<string?> ResponseTransferApp(Bot bot, EAccess access, string mode, string appIDs, string botNameTo, bool sendNotMarketable = true) {
+			if (string.IsNullOrEmpty(botNameTo) || string.IsNullOrEmpty(mode) || string.IsNullOrEmpty(appIDs)) {
+				ASF.ArchiLogger.LogNullError(null, nameof(mode) + " || " + nameof(botNameTo) + " || " + nameof(appIDs));
+
+				return null;
+			}
+
+			if (access < EAccess.Master) {
+				return null;
+			}
+
+			if (!bot.IsConnectedAndLoggedOn) {
+				return bot.Commands.FormatBotResponse(Strings.BotNotConnected);
+			}
+
+			Bot? targetBot = Bot.GetBot(botNameTo);
+
+			if (targetBot == null) {
+				return access >= EAccess.Owner ? bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNameTo)) : null;
+			}
+
+			if (!targetBot.IsConnectedAndLoggedOn) {
+				return bot.Commands.FormatBotResponse(Strings.BotNotConnected);
+			}
+
+			if (targetBot.SteamID == bot.SteamID) {
+				return bot.Commands.FormatBotResponse(Strings.BotSendingTradeToYourself);
+			}
+
+			string[] appIDTexts = mode.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+
+			if (appIDTexts.Length == 0) {
+				return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsEmpty, nameof(appIDTexts)));
+			}
+
+			HashSet<uint> realAppIDs = [];
+
+			foreach (string appIDText in appIDTexts) {
+				if (!uint.TryParse(appIDText, out uint appID) || (appID == 0)) {
+					return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, nameof(appID)));
+				}
+
+				realAppIDs.Add(appID);
+			}
+
+			string[] modes = mode.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+
+			if (modes.Length == 0) {
+				return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsEmpty, nameof(modes)));
+			}
+
+			HashSet<EAssetType> transferTypes = [];
+
+			foreach (string singleMode in modes) {
+				switch (singleMode.ToUpper(CultureInfo.CurrentCulture)) {
+					case "A":
+					case "ALL":
+						foreach (EAssetType type in (EAssetType[]) Enum.GetValues(typeof(EAssetType))) {
+							transferTypes.Add(type);
+						}
+
+						break;
+					case "BG":
+					case "BACKGROUND":
+						transferTypes.Add(EAssetType.ProfileBackground);
+
+						break;
+					case "BO":
+					case "BOOSTER":
+						transferTypes.Add(EAssetType.BoosterPack);
+
+						break;
+					case "C":
+					case "CARD":
+						transferTypes.Add(EAssetType.TradingCard);
+
+						break;
+					case "E":
+					case "EMOTICON":
+						transferTypes.Add(EAssetType.Emoticon);
+
+						break;
+					case "F":
+					case "FOIL":
+						transferTypes.Add(EAssetType.FoilTradingCard);
+
+						break;
+					case "G":
+					case "GEMS":
+						transferTypes.Add(EAssetType.SteamGems);
+
+						break;
+					case "U":
+					case "UNKNOWN":
+						transferTypes.Add(EAssetType.Unknown);
+
+						break;
+					default:
+						return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, mode));
+				}
+			}
+
+			(bool success, string message) = await bot.Actions.SendInventory(targetSteamID: targetBot.SteamID, filterFunction: item => transferTypes.Contains(item.Type) && (sendNotMarketable || item.Marketable) && realAppIDs.Contains(item.RealAppID)).ConfigureAwait(false);
+
+			return bot.Commands.FormatBotResponse(success ? message : string.Format(CultureInfo.CurrentCulture, Strings.WarningFailedWithError, message));
+		}
+
+		private static async Task<string?> ResponseTransferApp(EAccess access, ulong steamID, string botNames, string mode, string appIDs, string botNameTo, bool sendNotMarketable = true) {
+			if (string.IsNullOrEmpty(botNames) || string.IsNullOrEmpty(mode) || string.IsNullOrEmpty(botNameTo)) {
+				ASF.ArchiLogger.LogNullError(null, nameof(botNames) + " || " + nameof(mode) + " || " + nameof(botNameTo));
+
+				return null;
+			}
+
+			HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+			if ((bots == null) || (bots.Count == 0)) {
+				return access >= EAccess.Owner ? Commands.FormatStaticResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNames)) : null;
+			}
+
+			IEnumerable<Task<string?>> tasks = bots.Select(bot => ResponseTransferApp(bot, Commands.GetProxyAccess(bot, access, steamID), mode, appIDs, botNameTo, sendNotMarketable));
+			List<string?> results;
+
+			switch (ASF.GlobalConfig?.OptimizationMode) {
+				case GlobalConfig.EOptimizationMode.MinMemoryUsage:
+					results = new List<string?>(bots.Count);
+
+					foreach (Task<string?> task in tasks) {
+						results.Add(await task.ConfigureAwait(false));
+					}
+
+					break;
+				default:
+					results = [.. await Task.WhenAll(tasks).ConfigureAwait(false)];
+
+					break;
+			}
+
+			List<string?> responses = [..results.Where(static result => !string.IsNullOrEmpty(result))];
+
 			return responses.Count > 0 ? string.Join("", responses) : null;
 		}
 
 		private static async Task<string?> ResponseLoot(Bot bot, EAccess access, string mode, bool sendNotMarketable = true) {
+			if (string.IsNullOrEmpty(mode)) {
+				ASF.ArchiLogger.LogNullError(null, nameof(mode));
+
+				return null;
+			}
+
 			if (access < EAccess.Master) {
 				return null;
 			}
@@ -246,30 +415,37 @@ namespace SelectiveLootAndTransferPlugin {
 					case "BG":
 					case "BACKGROUND":
 						transferTypes.Add(EAssetType.ProfileBackground);
+
 						break;
 					case "BO":
 					case "BOOSTER":
 						transferTypes.Add(EAssetType.BoosterPack);
+
 						break;
 					case "C":
 					case "CARD":
 						transferTypes.Add(EAssetType.TradingCard);
+
 						break;
 					case "E":
 					case "EMOTICON":
 						transferTypes.Add(EAssetType.Emoticon);
+
 						break;
 					case "F":
 					case "FOIL":
 						transferTypes.Add(EAssetType.FoilTradingCard);
+
 						break;
 					case "G":
 					case "GEMS":
 						transferTypes.Add(EAssetType.SteamGems);
+
 						break;
 					case "U":
 					case "UNKNOWN":
 						transferTypes.Add(EAssetType.Unknown);
+
 						break;
 					default:
 						return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, mode));
@@ -280,7 +456,6 @@ namespace SelectiveLootAndTransferPlugin {
 
 			return bot.Commands.FormatBotResponse(success ? message : string.Format(CultureInfo.CurrentCulture, Strings.WarningFailedWithError, message));
 		}
-
 
 		private static async Task<string?> ResponseLoot(EAccess access, ulong steamID, string botNames, string mode, bool sendNotMarketable = true) {
 			if (string.IsNullOrEmpty(botNames)) {
@@ -297,11 +472,127 @@ namespace SelectiveLootAndTransferPlugin {
 
 			IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseLoot(bot, Commands.GetProxyAccess(bot, access, steamID), mode, sendNotMarketable))).ConfigureAwait(false);
 
-			List<string?> responses = new(results.Where(result => !string.IsNullOrEmpty(result)));
+			List<string?> responses = [..results.Where(static result => !string.IsNullOrEmpty(result))];
 
 			return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
 		}
 
+		private static async Task<string?> ResponseLootApp(Bot bot, EAccess access, string mode, string appIDs, bool sendNotMarketable = true) {
+			if (string.IsNullOrEmpty(mode) || string.IsNullOrEmpty(appIDs)) {
+				ASF.ArchiLogger.LogNullError(null, nameof(mode) + " || " + nameof(appIDs));
+
+				return null;
+			}
+
+			if (access < EAccess.Master) {
+				return null;
+			}
+
+			if (!bot.IsConnectedAndLoggedOn) {
+				return bot.Commands.FormatBotResponse(Strings.BotNotConnected);
+			}
+
+			if (bot.BotConfig.LootableTypes.Count == 0) {
+				return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsEmpty, nameof(Bot.BotConfig.LootableTypes)));
+			}
+
+			string[] appIDTexts = appIDs.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+
+			if (appIDTexts.Length == 0) {
+				return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsEmpty, nameof(appIDTexts)));
+			}
+
+			HashSet<uint> realAppIDs = [];
+
+			foreach (string appIDText in appIDTexts) {
+				if (!uint.TryParse(appIDText, out uint appID) || (appID == 0)) {
+					return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, nameof(appID)));
+				}
+
+				realAppIDs.Add(appID);
+			}
+
+			string[] modes = mode.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+
+			if (modes.Length == 0) {
+				return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsEmpty, nameof(modes)));
+			}
+
+			HashSet<EAssetType> transferTypes = [];
+
+			foreach (string singleMode in modes) {
+				switch (singleMode.ToUpper(CultureInfo.CurrentCulture)) {
+					case "A":
+					case "ALL":
+						foreach (EAssetType type in (EAssetType[]) Enum.GetValues(typeof(EAssetType))) {
+							transferTypes.Add(type);
+						}
+
+						break;
+					case "BG":
+					case "BACKGROUND":
+						transferTypes.Add(EAssetType.ProfileBackground);
+
+						break;
+					case "BO":
+					case "BOOSTER":
+						transferTypes.Add(EAssetType.BoosterPack);
+
+						break;
+					case "C":
+					case "CARD":
+						transferTypes.Add(EAssetType.TradingCard);
+
+						break;
+					case "E":
+					case "EMOTICON":
+						transferTypes.Add(EAssetType.Emoticon);
+
+						break;
+					case "F":
+					case "FOIL":
+						transferTypes.Add(EAssetType.FoilTradingCard);
+
+						break;
+					case "G":
+					case "GEMS":
+						transferTypes.Add(EAssetType.SteamGems);
+
+						break;
+					case "U":
+					case "UNKNOWN":
+						transferTypes.Add(EAssetType.Unknown);
+
+						break;
+					default:
+						return bot.Commands.FormatBotResponse(string.Format(CultureInfo.CurrentCulture, Strings.ErrorIsInvalid, mode));
+				}
+			}
+
+			(bool success, string message) = await bot.Actions.SendInventory(filterFunction: item => transferTypes.Contains(item.Type) && (sendNotMarketable || item.Marketable) && realAppIDs.Contains(item.RealAppID)).ConfigureAwait(false);
+
+			return bot.Commands.FormatBotResponse(success ? message : string.Format(CultureInfo.CurrentCulture, Strings.WarningFailedWithError, message));
+		}
+
+		private static async Task<string?> ResponseLootApp(EAccess access, ulong steamID, string botNames, string mode, string appIDs, bool sendNotMarketable = true) {
+			if (string.IsNullOrEmpty(botNames)) {
+				ASF.ArchiLogger.LogNullError(null, nameof(botNames));
+
+				return null;
+			}
+
+			HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+			if ((bots == null) || (bots.Count == 0)) {
+				return access >= EAccess.Owner ? Commands.FormatStaticResponse(string.Format(CultureInfo.CurrentCulture, Strings.BotNotFound, botNames)) : null;
+			}
+
+			IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseLootApp(bot, Commands.GetProxyAccess(bot, access, steamID), mode, appIDs, sendNotMarketable))).ConfigureAwait(false);
+
+			List<string?> responses = [..results.Where(static result => !string.IsNullOrEmpty(result))];
+
+			return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
+		}
 	}
 }
 #pragma warning restore CA1812 // ASF uses this class during runtime
